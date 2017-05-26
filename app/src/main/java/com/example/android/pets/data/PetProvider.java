cca589b312668eebfa5ae0c966525ddb306d0798
rsoni123 <Amaya123>
2017-05-26 17:07:32 -0400

    changes done

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -2,6 +2,7 @@ package com.example.android.pets.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -9,6 +10,36 @@ import android.net.Uri;
  * Created by pankaj on 5/26/2017.
  */
 public class PetProvider extends ContentProvider {
+
+    /**
+     * URI matcher code for the content URI for the pets table
+     */
+    private static final int PETS = 100;
+
+    /**
+     * URI matcher code for the content URI for a single pet in the pets table
+     */
+    private static final int PET_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        // TODO: Add 2 content URIs to URI matcher
+
+        sUriMatcher.addURI("content", "pets", PETS);
+        sUriMatcher.addURI("content", "pets/#", PET_ID);
+
+    }
 
     /**
      * Database helper object
@@ -23,6 +54,15 @@ public class PetProvider extends ContentProvider {
 
     @Override
     public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        switch (sUriMatcher.match(uri)) {
+            case PETS:
+                System.out.println("I am in Pets " + uri);
+                break;
+            case PET_ID:
+                System.out.println("I am in Pets Id " + uri);
+                break;
+        }
+
         return null;
     }
 
