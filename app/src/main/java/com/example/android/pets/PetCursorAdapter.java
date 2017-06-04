db752367842274f5968b76821fccdfc407e36142
rsoni123 <Amaya123>
2017-06-04 12:47:12 -0400

    Content Providers working

---
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -0,0 +1,46 @@
+package com.example.android.pets;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Created by pankaj on 6/4/2017.
+ */
+public class PetCursorAdapter extends CursorAdapter {
+
+    public PetCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    /**
+     * This method binds the pet data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current pet can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
